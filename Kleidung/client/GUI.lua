efd5ecfc986622ba017fcbe2ea09995d5fdce381
Lisa Flocke <44293842+LisaFlocke@users.noreply.github.com>
2019-03-15 15:15:30 +0100

    Update GUI.lua

--- Kleidung/client/GUI.lua
+++ Kleidung/client/GUI.lua
@@ -1,6 +1,6 @@
 --MIT License
 
---Copyright (c) [2019] [Staffen Koch | Lisa Flocke]
+--Copyright (c) 2019 LisaFlocke
 
 --Permission is hereby granted, free of charge, to any person obtaining a copy
 --of this software and associated documentation files (the "Software"), to deal
